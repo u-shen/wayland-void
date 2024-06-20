bdb7e23fd3c9f5d8068d9e6c67d71d8587e14cff
u-shen <farah.ushen@gmail.com>
2024-06-20 12:18:16 +0100

    Fix Git

--- void-dotfiles/cfg/nvim/lua/plugins/git.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/git.lua
@@ -96,7 +96,6 @@ return {
 				delay = 0,
 				ignore_whitespace = false,
 			},
-			current_line_blame_formatter_opts = { relative_time = true },
 			current_line_blame_formatter = "      <author>, <author_time:%R> - <summary>",
 			sign_priority = 6,
 			update_debounce = 100,
