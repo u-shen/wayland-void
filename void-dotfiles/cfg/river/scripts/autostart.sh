c49496a7c3ee82aceb7cd3f12b9f351626512d04
u-shen <farah.ushen@gmail.com>
2024-06-30 12:11:29 +0100

    enabled xwayland and add screenSharing

--- void-dotfiles/cfg/river/scripts/autostart.sh
+++ void-dotfiles/cfg/river/scripts/autostart.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #==========================#
+# Screen sharing:          #
+#==========================#
+exec dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river &
+#==========================#
 # Background Daemon:       #
 #==========================#
 killall wbg;wbg ~/pix/wallpapers/WITCHER.png &
