6912119476c26596cb0f90d96dee85e9f2618042
u-shen <farah.ushen@gmail.com>
2024-12-28 19:36:53 +0100

    change color to gruvbox-dark

--- void-dotfiles/cfg/wayland-suckless/dwlb/config.def.h
+++ void-dotfiles/cfg/wayland-suckless/dwlb/config.def.h
@@ -1,8 +1,8 @@
 #define HEX_COLOR(hex)                                                         \
-  {                                                                            \
-    .red = ((hex >> 24) & 0xff) * 257, .green = ((hex >> 16) & 0xff) * 257,    \
-    .blue = ((hex >> 8) & 0xff) * 257, .alpha = (hex & 0xff) * 257             \
-  }
+  {.red = ((hex >> 24) & 0xff) * 257,                                          \
+   .green = ((hex >> 16) & 0xff) * 257,                                        \
+   .blue = ((hex >> 8) & 0xff) * 257,                                          \
+   .alpha = (hex & 0xff) * 257}
 
 // use ipc functionality
 static bool ipc = false;
@@ -31,13 +31,13 @@ static char *tags_names[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 // set 16-bit colors for bar
 // use either pixman_color_t struct or HEX_COLOR macro for 8-bit colors
-static pixman_color_t active_fg_color = HEX_COLOR(0x282828ff);
+static pixman_color_t active_fg_color = HEX_COLOR(0x1d2021ff);
 static pixman_color_t active_bg_color = HEX_COLOR(0x89b482ff);
 static pixman_color_t occupied_fg_color = HEX_COLOR(0xD4BE98ff);
-static pixman_color_t occupied_bg_color = HEX_COLOR(0x282828ff);
+static pixman_color_t occupied_bg_color = HEX_COLOR(0x1d2021ff);
 static pixman_color_t inactive_fg_color = HEX_COLOR(0xD4BE98ff);
-static pixman_color_t inactive_bg_color = HEX_COLOR(0x282828ff);
-static pixman_color_t urgent_fg_color = HEX_COLOR(0x282828ff);
+static pixman_color_t inactive_bg_color = HEX_COLOR(0x1d2021ff);
+static pixman_color_t urgent_fg_color = HEX_COLOR(0x1d2021ff);
 static pixman_color_t urgent_bg_color = HEX_COLOR(0xea6962ff);
 static pixman_color_t middle_bg_color = HEX_COLOR(0x89b482ff);
 static pixman_color_t middle_bg_color_selected = HEX_COLOR(0x89b482ff);
