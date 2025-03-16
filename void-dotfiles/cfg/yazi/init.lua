333ebf0ae812700e77ce9d1d1b12fb759956a076
u-shen <farah.ushen@gmail.com>
2025-03-16 16:36:39 +0000

    fix yazi

--- void-dotfiles/cfg/yazi/init.lua
+++ void-dotfiles/cfg/yazi/init.lua
@@ -2,17 +2,13 @@
 -- Border Style:
 --------------------------------------------------
 require("full-border"):setup({
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.PLAIN,
+  -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+  type = ui.Border.PLAIN,
 })
 --------------------------------------------------
 -- Git integrations:
 --------------------------------------------------
 require("git"):setup()
---------------------------------------------------
--- Starship integrations
---------------------------------------------------
-require("starship"):setup()
 --------------------------------------------------
 -- Disable Status:
 --------------------------------------------------
