d43244cc1135ca5b8cbeca11c4f575580862e091
u-shen <farah.ushen@gmail.com>
2025-03-16 12:57:10 +0000

    sync extra packages

--- void-configs/void-extras.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-npm install -g pnpm yarn live-server typescript sass firebase-tools
---
+++ void-configs/win-extra.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ================================================#
+# List Packages:                                  #
+# ================================================#
+packages=(
+    # PACKAGE MANAGER:                             #
+    # =============================================#
+    "bun"
+    "yarn"
+    "pnpm"
+    # LANGUAGE SERVER PROTOCOL:                    #
+    # =============================================#
+    "typescript"
+    "typescript-language-server"
+    "vscode-langservers-extracted"
+    "@tailwindcss/language-server"
+    "@biomejs/biome"
+)
+# ================================================#
+# NPM Packages:                                    #
+# ================================================#
+for package in "${packages[@]}"; do
+    echo "Installing $package..."
+    npm install -g "$package"
+done
+echo "Installation Of NPM Packages Is Complete!"
