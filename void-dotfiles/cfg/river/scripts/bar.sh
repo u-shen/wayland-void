1b814cbd88bb3a9b403521aacc0ea8dc52b7d21d
u-shen <farah.ushen@gmail.com>
2025-03-26 22:36:02 +0000

    added tags

--- void-dotfiles/cfg/river/scripts/bar.sh
+++ void-dotfiles/cfg/river/scripts/bar.sh
@@ -5,6 +5,7 @@ FIFO="$XDG_RUNTIME_DIR/sandbar"
 mkfifo "$FIFO"
 
 while cat "$FIFO"; do :; done | sandbar \
+  -tags 9 " " " " " " " " " " " " " " " " " " \
 	-no-layout -hide-normal-mode \
 	-active-fg-color "#181616" \
 	-active-bg-color "#8a9a7b" \
